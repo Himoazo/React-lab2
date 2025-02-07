2135c05afc19dc82f0736390680e7f4ad5f97026
Hoomam <hoaz1600@student.miun.se>
2025-02-07 15:33:11 +0100

    Error handling & loader

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,69 @@
 import './App.css'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 interface TodosInterface {
-  _id: number,
-  todoTitle: string,
+  id: number,
+  todo_name: string,
   description: string,
   status: string
 }
 
-// States
-const [todos, setTodos] = useState<TodosInterface[] | []>([]);
+
+
+function App() {
+  // States
+  const [todos, setTodos] = useState<TodosInterface[] | []>([]);
+  const [error, setError] = useState<string | null>(null);
+  const [loader, setLoader] = useState<boolean>(false);
+useEffect(() => {
+  getTodos();
+}, []);
 
 const getTodos = async () => {
   try {
-    const response = await fetch("http://localhost:3000/api/");
+    setLoader(true);
 
+    const response = await fetch("http://localhost:3000/api/");
+    
     if (!response.ok) {
       throw Error;
     } else {
       const data = await response.json();
-
+      
       setTodos(data);
+      setError(null);
     }
   } catch (error) {
-    
+    console.log(error);
+    setError("Ett fel har inträffats");
+  } finally {
+    setLoader(false);
   }
 }
 
-function App() {
-  
-
   return (
-    <>
+    <main>
       <h1>Todo list</h1>
-    </>
+
+      {
+        error && <p className='errorMsg'>{error}</p>
+      }
+
+      {
+        loader && <p className=""> Hämtar todos...</p>
+      }
+      <div>
+        {
+          todos.map((todo) => (
+            <div key={todo.id}>
+              <h2>{todo.todo_name}</h2>
+              <p>{todo.description}</p>
+              <span>{todo.status }</span>
+            </div>
+          ))
+        }
+      </div>
+    </main>
   )
 }
 
