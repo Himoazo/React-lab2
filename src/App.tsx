c46bf16f20469e80b92600f9e6a2a2f754f49ffe
Hoomam <hoaz1600@student.miun.se>
2025-02-07 14:05:00 +0100

    Fetch todos

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,31 @@
 import './App.css'
+import { useState } from 'react'
+
+interface TodosInterface {
+  _id: number,
+  todoTitle: string,
+  description: string,
+  status: string
+}
+
+// States
+const [todos, setTodos] = useState<TodosInterface[] | []>([]);
+
+const getTodos = async () => {
+  try {
+    const response = await fetch("http://localhost:3000/api/");
+
+    if (!response.ok) {
+      throw Error;
+    } else {
+      const data = await response.json();
+
+      setTodos(data);
+    }
+  } catch (error) {
+    
+  }
+}
 
 function App() {
   
