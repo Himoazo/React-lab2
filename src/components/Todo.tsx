f7f2de84d6eb6219d09fcf1c61f7a1fdb82d3624
Hoomam <hoaz1600@student.miun.se>
2025-02-09 23:13:51 +0100

    Error handling

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,12 +1,14 @@
 import { TodosInterface } from "../Interfaces/TodosInterface";
+import { useState } from "react";
 
 const Todo = ({ todo, getTodos }: { todo: TodosInterface, getTodos: Function }) => {
-  
+  //Error state
+  const [error, setError] = useState<string | null>(null);
   const changeStat = async (event: any) => {
     const changedStat = { ...todo, status: Number(event.target.value) }
     
     try {
-      const response = await fetch("http://localhost:3000/api/" + todo.id, {
+      const response = await fetch("http://localhost:3000/apis/" + todo.id, {
         method: "PUT",
         headers: {
           "Content-type": "application/json"
@@ -19,8 +21,9 @@ const Todo = ({ todo, getTodos }: { todo: TodosInterface, getTodos: Function }) 
       } 
     
       getTodos();
+      setError(null);
     } catch (error) {
-      
+      setError("Ett fel har inträffats");
     }
   }
 
@@ -37,9 +40,10 @@ const Todo = ({ todo, getTodos }: { todo: TodosInterface, getTodos: Function }) 
         throw Error;
         } 
       
-        getTodos();
+      getTodos();
+      setError(null);
     } catch (error) {
-      
+      setError("Det gick inte att ta bort, försök igen");
     }
   }
 
@@ -49,7 +53,7 @@ const Todo = ({ todo, getTodos }: { todo: TodosInterface, getTodos: Function }) 
       <p>{todo.description}</p>
       <span>{todo.status === 0 ? "Ej påbörjad" : todo.status === 1 ? "Påbörkad"
         : todo.status === 2 ? "Avklarad" : "Ogilltig status"}</span>
-      <button onClick={()=>{deleteTodo(todo.id)}}>Ta bort</button>
+      
       <form>
         <label htmlFor="status">Uppdatera status</label>
         <select name="status" id="status" defaultValue={todo.status} onChange={changeStat}>
@@ -57,7 +61,10 @@ const Todo = ({ todo, getTodos }: { todo: TodosInterface, getTodos: Function }) 
           <option value="1">Påbörjad</option>
           <option value="2">Avklarad</option>
         </select>
-    </form>
+      </form>
+      <button onClick={() => { deleteTodo(todo.id) }}>Ta bort</button>
+      
+      {error && <p className='errorMsg'>{error}</p>}
     </div>
   )
 }
